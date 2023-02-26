f0f389b04aab3e8d412201a88d04c50d6e4094cd
Anant Girdhar <anant.girdhar@proton.me>
2023-02-26 14:15:22 -0500

    Add script to plot grid points

---
+++ plot_grid_points.py
@@ -0,0 +1,31 @@
+# coding: utf-8
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Read the file
+d = []
+with open('points', 'r') as f:
+    for line in f:
+        if line.startswith('('):
+            d.append(line)
+d = [line.strip() for line in d]
+
+# Extract the coordinates into separate lists
+X = []
+Y = []
+Z = []
+for line in d:
+    line = line.replace('(', '').replace(')', '')
+    x, y, z = line.split()
+    X.append(float(x))
+    Y.append(float(y))
+    Z.append(float(z))
+
+# Plot the coordinates
+fig = plt.figure()
+ax = fig.add_subplot(projection='3d')
+ax.scatter(X, Y, Z, marker='.')
+ax.set_xlabel('X')
+ax.set_ylabel('Y')
+ax.set_zlabel('Z')
+plt.show()
