6f99711c66c0d790710bc29eec45a2b5dfc6fc16
Anant Girdhar <anant.girdhar@proton.me>
2024-10-01 16:37:52 -0400

    Add python file to read and write OF solutions
    This python module provides functions to read an OpenFOAM solution into
    memory for further processing, as well as to write data back out as an
    OpenFOAM solution. My hope is to use this to be able to do some
    preprocessing of the data and write it back out to OpenFOAM format to be
    able to visualize it with paraFoam.

---
+++ postprocessing/rwopenfoam.py
@@ -0,0 +1,211 @@
+import pickle
+import textwrap
+import typing
+from pathlib import Path
+
+import numpy as np
+import numpy.typing as npt
+
+
+def _list_to_dimensions(dimargs: list[str]) -> list[int]:
+    if len(dimargs) != 7:
+        raise ValueError(
+            f"The dimensions string does not have 7 components: {dimargs}"
+        )
+    dimensions = []
+    for dim in dimargs:
+        if "[" in dim:
+            dimensions.append(int(dim.split("[")[-1]))
+        elif "]" in dim:
+            dimensions.append(int(dim.split("]")[0]))
+        else:
+            dimensions.append(int(dim))
+    return dimensions
+
+
+def _dimensions_to_str(dimensions: list[int]) -> str:
+    return str(dimensions).replace(",", "")
+
+
+def _sanitize_uniform_value(args: list[str]) -> float | tuple[float]:
+    if len(args) == 1:
+        # Just need to remove the trailing semicolon
+        return float(args[0].split(";")[0])
+    elif len(args) == 3:
+        # Process everything in between the parens and return a tuple
+        return (
+            float(args[0].split("(")[-1]),
+            float(args[1]),
+            float(args[2].split(")")[0]),
+        )
+
+
+def read_variable(file_path: Path) -> dict[str, typing.Any]:
+    data: dict[str, typing.Any] = {
+        "type": None,
+        "dimensions": None,
+        "data": [],
+    }
+    found_values_start = False
+    num_values = None
+    with open(file_path, "r") as infile:
+        for line in infile:
+            match line.split():
+                case ["class", field_type]:
+                    data["type"] = field_type[:-1]  # remove trailing semicolon
+                case ["dimensions", *args]:
+                    data["dimensions"] = _list_to_dimensions(args)
+                case ["internalField", "uniform", *args]:
+                    data["data"] = _sanitize_uniform_value(args)
+                    return data
+                case ["internalField", "nonuniform", *_]:
+                    # Found the start of the internalField
+                    # It is not a uniform field
+                    # Break out of this loop and process the values
+                    break
+        for line in infile:
+            match line.split():
+                case [num] if not found_values_start and num_values is None:
+                    num_values = int(num)
+                case ["("]:
+                    found_values_start = True
+                case [")"] if found_values_start:
+                    assert len(data["data"]) == num_values
+                    break
+                case [value] if found_values_start:
+                    data["data"].append(float(value))
+                case [vx, vy, vz] if found_values_start:
+                    data["data"].append(
+                        (
+                            float(vx.split("(")[-1]),
+                            float(vy),
+                            float(vz.split(")")[0]),
+                        )
+                    )
+    data["data"] = np.array(data["data"])
+    return data
+
+
+def openfoam_to_pickle(
+    timestamp: Path,
+) -> None:
+    solution: dict[str, npt.NDArray[np.float64] | float] = {}
+    for var_file in timestamp.iterdir():
+        if var_file.is_dir():
+            continue
+        var = var_file.name
+        solution[var] = read_variable(var_file)
+    pickle_filepath = Path(f"./solution_{timestamp}.p")
+    if pickle_filepath.exists():
+        raise FileExistsError(f"{pickle_filepath} already exists.")
+    with open(pickle_filepath, "wb") as pfile:
+        pickle.dump(solution, pfile)
+
+
+def _write_openfoam_var_file(
+    filepath: Path,
+    var: str,
+    values: dict[str, typing.Any],
+):
+    timestamp = filepath.parent.name
+    if values["type"] in ["volScalarField", "surfaceScalarField"]:
+        data_type = "scalar"
+    elif values["type"] in ["volVectorField"]:
+        data_type = "vector"
+    else:
+        raise ValueError(
+            f"Unknown data type {values['type']} for variable {var}"
+        )
+    with open(filepath, "w") as outfile:
+        # Write the header
+        outfile.write(
+            textwrap.dedent(
+                f"""\
+                FoamFile
+                {{
+                    version         2.0;
+                    format          ascii;
+                    arch            "LSB;label=32;scalar=64;";
+                    class           {values['type']};
+                    location        "{timestamp}";
+                    object          {var};
+                }}
+                // * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+                dimensions      {_dimensions_to_str(values['dimensions'])};
+
+                """
+            )
+        )
+        # Write the data
+        if isinstance(values["data"], float):
+            outfile.write(f"internalField   uniform {values['data']};\n\n")
+        elif isinstance(values["data"], tuple):
+            uniform_vector_value = str(values["data"]).replace(",", "")
+            outfile.write(
+                f"internalField   uniform {uniform_vector_value};\n\n"
+            )
+        elif isinstance(values["data"], np.ndarray):
+            outfile.write(f"internalField   nonuniform List<{data_type}>\n")
+            outfile.write(f"{len(values['data'])}\n")
+            outfile.write("(\n")
+            for v in values["data"]:
+                if isinstance(v, np.ndarray):
+                    v = f"({v[0]} {v[1]} {v[2]})"
+                else:
+                    v = str(v)
+                outfile.write(f"{v}\n")
+            outfile.write(")\n;\n\n")
+        # Write the footer
+        zero_value = "(0 0 0)" if data_type == "vector" else 0
+        outfile.write(
+            textwrap.dedent(
+                f"""\
+                boundaryField
+                {{
+                    fuel
+                    {{
+                        type        calculated;
+                        value       uniform {zero_value};
+                    }}
+                    air
+                    {{
+                        type        calculated;
+                        value       uniform {zero_value};
+                    }}
+                    outlet
+                    {{
+                        type        calculated;
+                        value       uniform {zero_value};
+                    }}
+                    frontAndBack
+                    {{
+                        type        empty;
+                    }}
+                }}
+                """
+            )
+        )
+
+
+def pickle_to_openfoam(
+    solution_pickle: Path,
+    timestamp: Path,
+) -> None:
+    with open(solution_pickle, "rb") as pfile:
+        data = pickle.load(pfile)
+    if timestamp.is_dir():
+        print(
+            f"The directory {timestamp} already exists. "
+            "Would you like to merge the loaded solution in?"
+        )
+        response = input("[y|N]: ")
+        if response.lower() not in ["y", "yes"]:
+            return
+    else:
+        timestamp.mkdir()
+    for var, values in data.items():
+        if (timestamp / var).is_file():
+            print(f"{var} already exists in {timestamp}. Skipping.")
+            continue
+        _write_openfoam_var_file(timestamp / var, var, values)
